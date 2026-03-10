200392e7cac91835b97bc2e8578a0296578ac7e8
aaryatopiwala <75400069+aaryatopiwala@users.noreply.github.com>
2026-03-09 22:05:50 -0700

    get times

--- src/gpu/GPU_LCS.cuh
+++ src/gpu/GPU_LCS.cuh
@@ -46,15 +46,24 @@ void GPUcalculateDistanceVector(
 
         // allocate host array for lcs lengths
         std::vector<uint32_t> h_lcs(n_seqs);
-
+		// start time
+		auto start_time = std::chrono::high_resolution_clock::now();
         // call non-template GPU worker (implemented in GPU_LCS.cu)
         computeLCSLengths(seq_to_ptr(ref), (CSequence**)sequences, n_seqs, h_lcs.data(), lcsbp);
-
+		// end time, print duration
+		auto end_time = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+		std::cout << "GPU computation time: " << duration.count() << " ms" << std::endl;
         // apply transform locally
         auto ref_len = seq_to_ptr(ref)->length;
         for (int i = 0; i < n_seqs; ++i) {
             out_vector[i] = transform(h_lcs[i], (uint32_t)ref_len, (uint32_t)seq_to_ptr(sequences[i])->length);
         }
+		
+		//end time 2 print duration
+		auto end_time_2 = std::chrono::high_resolution_clock::now();
+		auto duration_2 = std::chrono::duration_cast<std::chrono::milliseconds>(end_time_2 - end_time);
+		std::cout << "Host transform time: " << duration_2.count() << " ms" << std::endl;
     
 }
 
